28f6899b6018f6df8df1f87a5761cfecc5b0ef77
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-01-08 23:26:22 +0300

    radix Ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+import { Avatar, Box, Button, Card, Flex, Text } from '@radix-ui/themes';
+
 export default function Home() {
 	let a: number | string = 'salom';
 	console.log(a);
 
-	return <>salom</>;
+	return (
+		// <Card size='1' style={{ width: 350 }}>
+		// 	<Flex gap='3' align='center'>
+		// 		<Avatar size='3' radius='full' fallback='T' color='indigo' />
+		// 		<Box>
+		// 			<Text as='div' size='2' weight='bold'>
+		// 				Teodros Girmay
+		// 			</Text>
+		// 			<Text as='div' size='2' color='gray'>
+		// 				Engineering
+		// 			</Text>
+		// 		</Box>
+		// 	</Flex>
+		// </Card>
+		<Button variant='classic'>Get started</Button>
+	);
 }
